da69c274db2fba35c8bf7a8b1acadec0de2bcb28
JDTweddle <jdtweddle@gmail.com>
2024-08-21 11:00:18 +0100

    altered spacing on home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,9 +6,9 @@ const Home: React.FC = () => {
   return (
     <div className="relative min-h-screen flex flex-col items-center justify-center">
       <div className="text-center max-w-md mb-40 ml-20 mr-30 z-10">
-        <h1 className="text-5xl font-bold mb-10">John Tweddle</h1>
+        <h1 className="text-5xl font-bold mb-6">John Tweddle</h1>
         <p className="text-2xl mb-6 font-thin break-words">
-          Hi there, welcome. I'm a passionate cloud engineer.
+          I'm a passionate Cloud and Devops engineer.
         </p>
         <nav className="justify-center mb-10 flex flex-wrap">
           {['Home', 'About', 'Resume', 'Tools', 'Portfolio', 'Contact me'].map((item) => (
@@ -22,13 +22,13 @@ const Home: React.FC = () => {
           ))}
         </nav>
         <div className="justify-center flex space-x-6">
-          <a className="hover:text-yellow-400" href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+          <a className="hover:text-yellow-400" href="https://github.com/JDTweddle" target="_blank" rel="noopener noreferrer">
             <FaGithub size={35} />
           </a>
-          <a className="hover:text-yellow-400" href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+          <a className="hover:text-yellow-400" href="https://linkedin.com/in/john-tweddle-079219293" target="_blank" rel="noopener noreferrer">
             <FaLinkedin size={35} />
           </a>
-          <a className="hover:text-yellow-400" href="https://medium.com/@yourusername" target="_blank" rel="noopener noreferrer">
+          <a className="hover:text-yellow-400" href="https://medium.com/@jdtweddle" target="_blank" rel="noopener noreferrer">
             <FaMedium size={35} />
           </a>
         </div>
