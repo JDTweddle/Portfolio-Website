d53edfa72e2ebbfb2716e3ad80fa11353e903a1f
JDTweddle <149077184+JDTweddle@users.noreply.github.com>
2024-09-11 15:38:46 +0100

    change values for state.tf

--- terraform/state.tf
+++ terraform/state.tf
@@ -1,9 +1,9 @@
 
 terraform {
     backend "s3" {
-        bucket = "patchez-terraform-state"
+        bucket = "your-bucket-name"
         key = "global/s3/terrafrom.tfstate"
-        region = "eu-west-2"
+        region = "your region"
         
     }
 }
