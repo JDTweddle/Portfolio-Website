a34dd34d1c0bae2f3c737d6ac88277f4f844784e
JDTweddle <149077184+JDTweddle@users.noreply.github.com>
2024-09-11 16:23:13 +0100

    changing variables in variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,17 +3,17 @@
 
 variable "aws_region" {
   description = "The AWS region to create resources in."
-  default     = "eu-west-2"
+  default     = "your-region"
 }
 
 variable "domain_name" {
   description = "The domain name for the website"
-  default     = "johntweddle.net"
+  default     = "your-domain-name"
 }
 
 # Name for static web bucket
 variable "bucket_name" {
   description = "name of bucket for static website"
-  default     = "patchez-portfolio-bucket"
+  default     = "you-bucket-name"
 }
 
